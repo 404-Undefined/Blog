ade4e72b64b169af69248efd38ef85db82213601
Steven <stevenohohohoh@gmail.com>
2020-08-04 20:50:01 +0900

    fixed

--- application/config.py
+++ application/config.py
@@ -4,9 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+	
 	SECRET_KEY = os.getenv("SECRET_KEY")
 	SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
+	SECRET_KEY = "07042cf406df6e1cf5fa3554da062604"
+	
 
 	MAIL_SERVER = "smtp.googlemail.com"
 	MAIL_PORT = 587
