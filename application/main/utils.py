13a5992e2fc7e2ea6c5f351e8bae46fb167e02c0
SoySoy4444 <sshoinkyuraa.08.04@hotmail.co.jp>
2020-08-07 11:37:54 +0900

    Implemented multithreading to emails

--- application/main/utils.py
+++ application/main/utils.py
@@ -1,6 +1,8 @@
 import os
 from flask_mail import Message
 from application import mail
+from flask import current_app
+import threading
 
 def send_confirmation_email(name, email):
 	msg = Message(
@@ -15,15 +17,20 @@ def send_confirmation_email(name, email):
 		)
 	mail.send(msg)
 
-def send_everyone_email(subject, recipients, content):
-	msg = Message(
-		subject, 
-		sender=os.getenv("MAIL_USERNAME"), 
-		recipients=recipients,
-		html=content,
-		)
+def send_email(app, subject, recipient, content):
+	with app.app_context():
+		msg = Message(subject, sender=os.getenv("MAIL_USERNAME"), recipients=[recipient], html=content)
+		mail.send(msg)
 
-	mail.send(msg)
+def send_everyone_email(subject, recipients, content):
+	threads = []
+	for recipient in recipients:
+		app = current_app._get_current_object()
+		t = threading.Thread(target=send_email, args=[app, subject, recipient, content])
+		t.start()
+		threads.append(t)
+	for thread in threads:
+		thread.join()
 
 def send_test_email(subject, content):
 	msg = Message(subject=subject, sender=os.getenv("MAIL_USERNAME"), recipients=[os.getenv("MAIL_USERNAME")], html=content)
